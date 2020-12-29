41e8e1ae107e4c9a753789264db14cb4003b8cb4
Adam Thorne <ahthornev2@gmail.com>
2020-12-29 19:24:43 +0000

    Edit folder/file structure to initialize project

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import './Application.css';
 
-function App() {
+export default function Application() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +20,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+};
