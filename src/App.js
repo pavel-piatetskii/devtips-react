177c554e918b903d792614c4aafa10bab5921b2c
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-12-29 14:06:38 -0800

    Add API connection with local:8080 proxy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+
+  const refresh = false;
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('/tips/?tipIDs=[1,2,3]')
+    ]).then(all => {
+      console.log(all)
+      })
+    }, [refresh])
+
   return (
     <div className="App">
       <header className="App-header">
