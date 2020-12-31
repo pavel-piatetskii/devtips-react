db048e1af003a61de06c9034a4b5b3bff0311376
Adam Thorne <ahthornev2@gmail.com>
2020-12-31 05:27:26 +0000

    Edit style of text-area, color-scheme, and alignment

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
+// import axios from "axios";
 
 import Header from "./Header";
 
 import "./Application.scss";
 
 export default function Application() {
-
+  /*
   const stub = false;
-
+  
   useEffect(() => {
     Promise.all([
       axios.get('/tips/?tipIDs=[1,2,3]')
@@ -16,6 +16,7 @@ export default function Application() {
       console.log(all)
       })
     }, [stub]);
+    */
 
   return (
     <main className="layout">
--- src/components/Header/Register.js
+++ src/components/Header/Register.js
@@ -8,9 +8,18 @@ import "./LoginRegister.scss";
 export default function Register(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
   return (
     <main className="__form">
+      <input
+        className="__form--input"
+        name="name"
+        type="text"
+        placeholder="Jim Smith"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
       <input
         className="__form--input"
         name="email"
