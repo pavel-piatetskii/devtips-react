8da3e158f16b2fb8900ee747caa7fc1421946aab
Adam Thorne <ahthornev2@gmail.com>
2020-12-31 01:19:46 +0000

    Edit login components for header, utilizing transitions and useVisualMode

---
+++ src/components/Header/Register.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+import back from "images/back.png";
+import Button from "components/Button";
+
+import "./LoginRegister.scss";
+
+export default function Register(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <main className="__form">
+      <input
+        className="__form--input"
+        name="email"
+        type="email"
+        placeholder="yourname@email.com"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <input
+        className="__form--input"
+        name="password"
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <section className="__form--buttons">
+        <img 
+          className="__form--back"
+          src={back}
+          alt="Back"
+          onClick={props.onBack}
+        />
+        <Button submit onClick={props.onRegister}>Register</Button>
+      </section>
+    </main>
+  );
+};
